33357787108abf142717d68874c5cf9289df197f
Bohan Yang <youthdna@live.com>
2019-07-17 10:53:02 +0800

    Update

--- Config.php
+++ Config.php
@@ -12,7 +12,7 @@ function get_env($varname)
 $config = array(
     'appId' => get_env('LEANCLOUD_APP_ID'),
     'appKey' => get_env('LEANCLOUD_APP_KEY'),
-    'apiUrl' => get_env('LEANCLOUD_API_URL') ?: 'https://leanapi.bohan.co/1.1/',
+    'apiUrl' => get_env('LEANCLOUD_API_URL') ?: 'https://avoscloud.com/1.1/',
     'cdnImage' => get_env('APP_CDN_IMAGE') ?: 'https://www.bing.com',
     'cdnVideo' => get_env('APP_CDN_VIDEO') ?: 'https://az29176.vo.msecnd.net',
     'size' => get_env('APP_SIZE_IMAGE') ?: '1920x1080',
